0a51e21368099ee9e4a0d3cae4aa7bb56ea49e0d
romerodny <romerodny@gmail.com>
2019-02-23 21:26:30 -0800

    Introduced a very simple interface for an image handler. Currently, all it really has to do is get and image from some source and return some attachment type. Also introduced a handler factory. Big brained ideas coming up.

---
+++ ImageHandler/Factory/ImageHandlerFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using objpatrishbot.ImageHandler.Interface;
+
+namespace objpatrishbot.ImageHandler.Factory
+{
+    class ImageHandlerFactory <TAttachment>
+    {
+        public IImageHandler<TAttachment> Create(string user, string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ImageHandler/Interface/IImageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace objpatrishbot.ImageHandler.Interface
+{
+    // TAttachment is the return type of the attachment.
+    interface IImageHandler <TAttachment>
+    {
+        /// <summary>
+        /// Gets a random image of a specified user.
+        /// </summary>
+        /// <param name="user">User's name</param>
+        /// <returns>An image of type TAttachment</returns>
+        TAttachment GetImage(string user);
+
+    }
+}
